900e3a4a3483db0ba5b959d39e6e4f4337a9d4b2
yasui05821 <yasui05821@gmail.com>
2018-02-07 19:00:52 +0900

    Serviceセクション　sp修正

--- components/Page/Index/Service.vue
+++ components/Page/Index/Service.vue
@@ -19,23 +19,23 @@
         <li class="p-service__problemListsItem">複雑なサイトを技術で解決したい</li>
         <li class="p-service__problemListsItem">SPAにしたい</li>
       </ul>
-    </div>
-    <div class="p-service__description">
-      <h3 class="p-service__descriptionTitle">複雑化するシステム構築の課題を、 最新の制作技術でしっかりサポート</h3>
-      <div class="p-service__descriptionText">
-        <span class="text-blod">複雑化するシステム構築の課題</span>を、
-        <span class="text-blod">最新の制作技術でしっかりサポート</span>多様化するニーズや、複雑化するシステム構築の課題を、最新の制作技術でしっかりサポート。
-        <span class="text-blod">Web制作に関するあらゆる相談に対応出来るよう、経験豊富なスタッフが親身に対応にあたります。</span>
+      <div class="p-service__description">
+        <h3 class="p-service__descriptionTitle">複雑化するシステム構築の課題を、 最新の制作技術でしっかりサポート</h3>
+        <div class="p-service__descriptionText">
+          <span class="text-blod">複雑化するシステム構築の課題</span>を、
+          <span class="text-blod">最新の制作技術でしっかりサポート</span>多様化するニーズや、複雑化するシステム構築の課題を、最新の制作技術でしっかりサポート。
+          <span class="text-blod">Web制作に関するあらゆる相談に対応出来るよう、経験豊富なスタッフが親身に対応にあたります。</span>
+        </div>
       </div>
-    </div>
-    <div class="p-service__example">
-      <div class="p-service__exampleHeading">導入事例</div>
-      <ul class="p-service__exampleList">
-        <li class="p-service__exampleListItem">Webサイト制作</li>
-        <li class="p-service__exampleListItem">Webサイト制作</li>
-        <li class="p-service__exampleListItem">Webサイト制作</li>
-        <li class="p-service__exampleListItem">Webサイト制作</li>
-      </ul>
+      <div class="p-service__example">
+        <div class="p-service__exampleHeading">導入事例</div>
+        <ul class="p-service__exampleList">
+          <li class="p-service__exampleListItem">Webサイト制作</li>
+          <li class="p-service__exampleListItem">Webサイト制作</li>
+          <li class="p-service__exampleListItem">Webサイト制作</li>
+          <li class="p-service__exampleListItem">Webサイト制作</li>
+        </ul>
+      </div>
     </div>
   </section>
 </template>
@@ -65,16 +65,16 @@
         content: "01";
         text-align: center;
         line-height: 2.3;
-        font-size: 1.4rem;
+        font-size: 1.8rem;
         color: #fff;
-        width: 50px;
-        height: 50px;
+        width: 70px;
+        height: 70px;
         border-radius: 100%;
         background: #C3504F;
         position: absolute;
-        top: -30px;
+        top: -35px;
         left: 50%;
-        transform: translateX(-25px);
+        transform: translateX(-35px);
         z-index: -10;
         @include desktop {
           border: none;
@@ -96,22 +96,38 @@
       box-sizing: border-box;
       padding: 0 3rem;
       width: 100%;
-      height: 150px;
+      height: 100px;
       background: linear-gradient(left, rgba(195,80,79,0.7) 0%, rgba(195,80,79,0.7) 1%, rgba(195,80,79,0.01) 98%, rgba(195,80,79,0) 100%);;
+      @include desktop {
+        height: 150px;
+      }
     }
     &__headingTitleWrap {
       color: #fff;
-      line-height: 5;
+      @include desktop {
+        line-height: 5;
+      }
     }
     &__headingTitle {
-      display: inline-block;
-      font-size: 3rem;
+      display: block;
+      padding-top: 2.2rem;
+      font-size: 2.5rem;
       font-weight: bold;
-      vertical-align: middle;
+      @include desktop {
+        display: inline-block;
+        font-size: 3rem;
+
+      }
     }
     &__headingSubTitle {
-      margin-left: 1.5rem;
-      display: inline-block;
+      display: block;
+      margin-left: 0;
+      font-size: 1.4rem;
+      font-size: 1.4rem;
+      @include desktop {
+        display: inline-block;
+        margin-left: 1.5rem;
+      }
     }
     &__contentsInner{
       @include c-container;
@@ -120,29 +136,52 @@
       display: flex;
       justify-content: space-between;
       box-sizing: border-box;
-      margin-bottom: 40px;
-
+      margin-bottom: 15px;
+      align-items: flex-start;
+      flex-wrap: wrap;
+      @include desktop {
+        flex-wrap: nowrap;
+        margin-bottom: 40px;
+      }
     }
     &__problemListsItem  {
+      margin: 0 1px;
       padding: 1.3rem 2.5rem;
       width: auto;
       text-align: center;
+      font-size: 1.1rem;
       font-weight: bold;
       border-radius: 4px;
       background: #fbf3f3;
       position: relative;
+
+      &:not(:last-of-type){
+        margin-bottom: 2rem;
+      }
+
+      @include desktop {
+        margin: 0 2px;
+        margin-bottom: 0;
+        font-size: 1.5rem;
+      }
+
       &::after {
         content: "";
         display: inline-block;
         width: 0;
         height: 0;
         border-style: solid;
-        border-width: 14px 12px 0 12px;
+        border-width: 10px 8px 0 8px;
         border-color: #fbf3f3 transparent transparent transparent;
         position: absolute;
-        bottom: -13px;
+        bottom: -8px;
         left: 50%;
-        transform: translateX(-6px);
+        transform: translateX(-4px);
+        @include desktop {
+          border-width: 14px 12px 0 12px;
+          bottom: -13px;
+          transform: translateX(-6px);
+        }
       }
     }
     &__description {
@@ -162,21 +201,43 @@
       display: flex;
       margin-bottom: 40px;
       align-items:center;
+      flex-wrap: wrap;
+      @include desktop {
+        flex-wrap: nowrap;
+      }
     }
     &__exampleHeading {
+      box-sizing: border-box;
+      margin-bottom: 1rem;
       padding: 1rem 4rem;
+      width: 100%;
       background: #fbf3f3 ;
+      text-align: center;
+      font-size: 1.4rem;
+      @include desktop {
+        margin-bottom: 0;
+        padding: 1rem 4rem;
+        width: auto;
+        font-weight: normal;
+      }
     }
     &__exampleList {
       box-sizing: border-box;
-      padding-left: 20px;
+      padding-left: 0;
+      @include desktop {
+        padding-left: 20px;
+      }
     }
     &__exampleListItem {
       display: inline-block;
+      font-weight:bold;
+      @include desktop {
+
+      }
       &::after {
         display: inline-block;
         content: "/";
-        padding: 1rem;
+        padding: .5rem 1rem;
       }
     }
   }
