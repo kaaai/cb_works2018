1b0553335881921d40729260135e90eff6460bb2
yasui05821 <yasui05821@gmail.com>
2018-02-07 14:00:07 +0900

    Footer　ブランドサイトと同じように実装

---
+++ components/Contact.vue
@@ -0,0 +1,251 @@
+<template>
+  <section class="c-footer">
+    <div class="c-contact_info c-balloon_nozzle">
+      <div>
+        <i class="fa fa-envelope-o" aria-hidden="true"></i>
+        お問い合わせはこちら
+      </div>
+    </div>
+    <div class="c-content_contact c-content_contact_bg c-footer_inner">
+      <span class="square"></span>
+      <div class="c-contact_title">Webにまつわるお悩み、<br>ご相談はお気軽にchatboxまで。</div>
+      <div class="c-form_btn">
+        <div>
+
+            <span>お問い合わせフォームへ</span>
+          <i class="fa fa-angle-right fa-lg" aria-hidden="true"></i>
+        </div>
+      </div>
+      <div class="c-contact_tel">
+        <div>お電話でのお問い合わせ</div>
+        <a href="tel:050-2555-1212">050-2555-1212</a>
+      </div>
+    </div>
+    <ul>
+      <li><a href="#">chatboxについて</a></li>
+      <li><a href="#">サービス</a></li>
+      <li><a href="#">メンバー</a></li>
+      <li><a href="#">お問い合わせ</a></li>
+      <div></div>
+    </ul>
+    <div class="c-footer_content c-footer_inner">
+      <div class="c-chatbox_copy">
+        <i class="fa fa-window-close-o fa-lg" aria-hidden="true"></i>
+        <small>Copyright © chatbox.inc All Rights Reserved.</small>
+      </div>
+      <div class="c-sns_content">
+        <i class="fa fa-facebook fa-lg" aria-hidden="true"></i>
+        <i class="fa fa-instagram fa-lg" aria-hidden="true"></i>
+      </div>
+    </div>
+
+  </section>
+</template>
+
+<script>
+  export default {
+  }
+
+</script>
+
+<style lang="scss" scoped>
+  .c-footer {
+    text-align: center;
+    overflow: hidden;
+  }
+  .c-footer_inner{
+    padding: 0 3% 3%;
+  }
+  a{
+    color: #C3504F;
+    &:hover{
+      text-decoration: none;
+      opacity: 0.7;
+    }
+  }
+  .c-contact_info{
+    padding-top: 14px;
+    color: #B43B3E;
+    position: fixed;
+    height: 50px;
+    z-index: 1000;
+    width: 100%;
+    bottom: 0;
+    background-color: #fff;
+  }
+  .c-content_contact{
+    color: #fff;
+    letter-spacing: 2px;
+    div:nth-child(1){
+      font-size: 110%;
+      line-height: 1.8;
+      margin: 10% 0 5%;
+    }
+  }
+  .c-content_contact_bg{
+    background-color: #C3504F;
+    span.square{
+      display: inline-block;
+      vertical-align: top;
+      height: 0;
+      border: 27px solid transparent;
+      border-top: 27px solid #fff;
+      font-weight: 600;
+    }
+    & c-form_btn{
+      margin-bottom: 4%;
+    }
+  }
+
+  .c-form_btn{
+    margin: 0 auto;
+    position: relative;
+    span{
+      padding: 5%;
+      display: inline-block;
+      font-weight: 600;
+      font-size: 77%;
+      vertical-align: super;
+      @media screen and (min-width: 768px) {
+        font-size: 120%;
+        padding: 7% 12% 7% 7%;
+      }
+    }
+    & i{
+      margin-top: 6%;
+      padding-left: 9%;
+      font-size: 120%;
+      @media screen and (min-width: 768px) {
+        font-size: 250%;
+        vertical-align: super;
+      }
+    }
+    div{
+      border-radius: 6px;
+      background-color: #fff;
+      color: #C3504F;
+      width: 90%;
+      display: inline-block;
+    }
+  }
+  .c-contact_tel{
+    margin: 5%;
+    div:nth-child(1){
+      margin: 0;
+    }
+    a{
+      color: #fff;
+      font-size: 180%;
+    }
+  }
+  ul.footer li {
+    display: inline;
+    text-align: center;
+    a{
+      color: #C3504F;
+    }
+  }
+  ul{
+    list-style: none;
+    margin-top: 3%;
+    padding: 0 3%;
+    div{
+      border-bottom: solid 1px #C3504F;
+      width: 100%;
+      margin: 4% auto 0;
+    }
+  }
+  ul li:first-child{
+      padding-left: 0;
+  }
+  li {
+    display: inline;
+    text-align: center;
+    width:(100% / 4);
+    color: #C3504F;
+    font-size: 84%;
+    padding-left: 4%;
+    font-weight: 600;
+
+  }
+  .c-footer_content{
+    color: #C3504F;
+    small{
+      font-size: 50%;
+      padding-left: 2%;
+    }
+  }
+  .c-chatbox_copy{
+    text-align: left;
+    width: 70%;
+    display: inline-block;
+  }
+  .c-sns_content{
+    text-align: right;
+    width: 28%;
+    display: inline-block;
+    i{
+      margin-left: 10%;
+    }
+  }
+  /* pc */
+  @media screen and (min-width: 768px) {
+      /* footer */
+    .c-footer_inner{
+    padding: 0 3% 5%;
+    }
+    .c-contact_info{
+      font-weight: normal;
+      font-size: 110%;
+      margin: 1% 0;
+      padding-left: 2%;
+      i{
+        padding-right: 1%;
+      }
+    }
+    .c-contact_title{
+      font-size: 180%;
+      margin-top: 4%;
+      letter-spacing: 3px;
+    }
+    .c-form_btn{
+      margin-left: 13%;
+      width: 47%;
+      display: inline-block;
+      span{
+        font-weight: 600;
+      }
+      & div{
+        padding-bottom: 0;
+      }
+      i{
+        padding-left: 0;
+        vertical-align: baseline;
+      }
+    }
+    .c-contact_tel{
+      text-align: left;
+      vertical-align: middle;
+      width: 30%;
+      display: inline-block;
+      margin: 0 0 2% 2%;
+      font-size: 110%;
+      letter-spacing: 4px;
+    }
+    ul{
+      text-align: left;
+      li{
+        font-weight: normal;
+      }
+      div{
+        margin: 2% 0;
+      }
+    }
+    .c-footer_content small{
+      font-size: 80%;
+    }
+    .c-sns_content i{
+      margin-left: 4%;
+    }
+  }
+</style>
