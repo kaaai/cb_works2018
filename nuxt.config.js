180d0cce4939ff29ee800319ffce326fca2d88b5
yasui05821 <yasui05821@gmail.com>
2018-02-07 11:34:09 +0900

    head　title viewportなど記載

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,10 +8,16 @@ module.exports = {
   ** Common headers are already provided by @nuxtjs/pwa preset
   */
   head: {
-    link: [
-      { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css' },
-      { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' },
-    ]
+      titleTemplate: '株式会社chatbox',
+      meta: [
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, minimum-scale=1, initial-scale=1, user-scalable=no' },
+          { hid: 'description', name: 'description', content: 'Meta description' },
+      ],
+      link: [
+          { rel: 'shortcut icon', href: '/favicon.ico' },
+          { rel:'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'}
+      ]
   },
   css: [],
   /*
